908304cbdf0e136c7cce8c14df6d1f6dec74d3f8
FolkusOnMe <94175734+FolkusOnMe@users.noreply.github.com>
2022-06-15 20:28:18 +0800

    15 June
    Adding problem 1 from leetcode and also made java file for mergeSort

---
+++ Leetcode/1_TwoSum.java
@@ -0,0 +1,58 @@
+package Leetcode;
+
+/**
+ * Given an array of integers `nums` and an integer `target`, return indices of 
+ * the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * e.g. 1 
+ *    Input: nums = [2,7,11,15], target = 9 
+ *    Output: [0,1] 
+ *    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * e.g. 2
+ *    Input: nums = [3,2,4], target = 6 
+ *    Output: [1,2] 
+ * 
+ * e.g. 3
+ *    Input: nums = [3,3], target = 6
+ *    Output: [0,1]
+ * 
+ * Constraints:
+ *  2 <= nums.length <= 104
+ *  -109 <= nums[i] <= 109
+ *  -109 <= target <= 109
+ *  Only one valid answer exists.
+ * 
+ * 
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ * 
+ */
+
+
+public class TwoSum {
+    
+
+    public int[] twoSum(int[] nums, int target){
+        
+        //solution without sorting
+        int size = nums.length;
+        int[] answer = new int[2];
+                
+        for(int i=0; i<size; i++){ //O(n)
+            for(int j=0; j<size; j++){ // * O(n)
+                if(j != i && nums[j] + nums[i] == target){
+                    answer[0] = j;
+                    answer[1] = i;
+                    break;
+                }
+            }
+        }
+        return answer;   
+    }
+
+
+    
+}
